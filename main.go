a37e67fb633ea3a191b480e7ce4bd7daedaea33a
FelipeRomao <feliperomao.a@gmail.com>
2023-08-28 19:00:12 -0300

    refactor: :recycle: remove unnecessary infiniteCount func

--- main.go
+++ main.go
@@ -6,19 +6,11 @@ import (
 	"time"
 )
 
-func infiniteCount(thing string) {
-	for i := 1; true; i++ {
-		fmt.Println(i, thing)
-		time.Sleep(time.Second * 1)
-	}
-}
-
 func count(thing string) {
 	for i := 1; i <= 5; i++ {
 		fmt.Println(i, thing)
 		time.Sleep(time.Millisecond * 500)
 	}
-
 }
 
 func main() {
